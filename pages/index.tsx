7da104084ca7aebc4105d594edd91acab20bc70e
Ishan Panchamia <46631742+Blueflame100@users.noreply.github.com>
2023-03-07 00:13:10 -0500

    overrided Image error

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "../styles/Home.module.css";
 
 function MainPage() {
   return (
+  <>
     <div>
       <Head>
         <title>HexLabs - Catalyst</title>
@@ -167,9 +168,9 @@ function MainPage() {
           </div>
         </section>
         <section id="schedule" className={styles.sectionSchedule}>
-          <Image src="/img/stars.png"></img>
+          <Image src="/img/stars.png"/>
           <h3 className={styles.sectionTitle}>Schedule COMING SOON!</h3>
-          <Image src="/img/roses.png"></img>
+          <Image src="/img/roses.png"/>
         </section>
         <section id="faq" className={styles.sectionFAQ}>
           <h3 className={styles.sectionTitle}>FAQ</h3>
@@ -238,6 +239,7 @@ function MainPage() {
         </footer>
       </div>
     </div>
+    </>
   );
 }
 
